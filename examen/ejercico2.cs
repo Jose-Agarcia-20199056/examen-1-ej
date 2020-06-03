e9555ea9d513322743a82c00a9c2b857a3117f27
JoseAgarcia2019 <20199056@itla.edu.do>
2020-06-03 02:25:51 +0000

    .2

---
+++ examen/ejercico2.cs
@@ -0,0 +1,32 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+		
+    // Dias de la semana o fin de semana 
+		/* profesor le escribi lo que me sucedio por dm que rep.it
+    me salio que estaba en manteniento per se salio de las manos no envialor a tiempo bueno pero gracias de todos modos pase lo que pase.
+
+    */
+    
+	{
+		Console.WriteLine("Escriba un dia de la semana");
+		  
+		  String seman = Console.ReadLine();
+	      
+		
+		if (seman == "domingo" ||  seman == "sabado" )
+		{
+		  Console.WriteLine("Es fin de semana ");
+		  
+		}	
+	
+	     else 
+		 {
+		    Console.WriteLine(" no es fin de semana ");
+		  
+		 }	 
+	}
+	
+}
